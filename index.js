fd0cd369546b41e72b4ea4718cc6125a4c446af5
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-10-26 17:41:41 +0530

    Add modal with close functionality
    Introduced a modal to the UI that displays after 1.5 seconds and can be closed via a button. This enhances user interaction by allowing users to dismiss the modal.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const modal = document.getElementById('modal')
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
+
+const closeButton = document.getElementById('modal-close-btn');
+
+closeButton.addEventListener("click", function() {
+    modal.style.display = 'none'
+})
