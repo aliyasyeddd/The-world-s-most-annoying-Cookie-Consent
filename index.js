f7578a07a2879e0e6b7753b93db0b44830e8a693
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-10-28 10:16:05 +0530

    Add consent form submit handler to modal
    Added an event listener to prevent default submission of the consent form in the modal. Also improved code formatting and organization for modal open/close logic.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const modal = document.getElementById('modal')
+const closeButton = document.getElementById('modal-close-btn');
+const form = document.getElementById("consent-form")
 
-setTimeout(function(){
+
+setTimeout(function () {
     modal.style.display = 'inline'
 }, 1500)
 
-const closeButton = document.getElementById('modal-close-btn');
 
-closeButton.addEventListener("click", function() {
+
+closeButton.addEventListener("click", function () {
     modal.style.display = 'none'
+})
+
+
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault()
 })
