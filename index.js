a7204e2810d0f44ae89d1b295594298067c7f832
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-10-28 10:47:53 +0530

    Refactor form variable and add sale status update
    Renamed the form variable to consentForm for clarity and added a setTimeout to update the upload status text after 1.5 seconds when the form is submitted.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const modal = document.getElementById('modal')
 const closeButton = document.getElementById('modal-close-btn');
-const form = document.getElementById("consent-form")
+const consentForm = document.getElementById("consent-form")
 const modalText = document.getElementById('modal-text')
 
 
@@ -14,13 +14,16 @@ closeButton.addEventListener("click", function () {
 })
 
 
-form.addEventListener("submit", function (e){
-    e.preventDefault()
+consentForm.addEventListener("submit", function (e) {
+    e.preventDefault();
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
         <p id="upload-text">Uploading your data to the dark web...</p>
     </div>`
+    setTimeout(function () {
+        document.getElementById("upload-text").innerText = "Making the sale..."
+    }, 1500)
 })
 
 
